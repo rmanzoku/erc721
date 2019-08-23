d685c0d4dfbc9eb18d9aa04cc5c9d1521b4c3c88
Ryo Manzoku <manzoku.ryo@gmail.com>
2019-08-23 16:53:14 +0900

    Bulkminter

--- contracts/solc0.5/ERC721BulkMinter.sol
+++ contracts/solc0.5/ERC721BulkMinter.sol
@@ -8,31 +8,54 @@ interface ERC721Mintable {
 
 contract ERC721BultMinter {
 
-  ERC721Mintable Alice;
-  address operator;
+  ERC721Mintable erc721;
+  address public operator;
+  address payable public owner;
 
   modifier onlyOperator() {
-    require(msg.sender == operator);
+    require((msg.sender == operator || msg.sender == owner));
+    _;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
     _;
   }
 
   constructor() public {
-    operator = msg.sender;
+    owner = msg.sender;
   }
 
   function renounce() public onlyOperator() {
-    Alice.renounceMinter();
+    erc721.renounceMinter();
   }
 
-  function set(address _new) external onlyOperator() {
-    Alice = ERC721Mintable(_new);
-    require(Alice.isMinter(address(this)), "address(this) is must be minter");
+  function setOpeartor(address _operator) external onlyOwner() {
+    operator = _operator;
+  }
+
+  function isMinter(address _target) public view returns (bool) {
+    ERC721Mintable target = ERC721Mintable(_target);
+    return target.isMinter(address(this));
+  }
+
+  function getContract() public view returns (address) {
+    return address(erc721);
+  }
+
+  function setContract(address payable _new) external onlyOperator() {
+    erc721 = ERC721Mintable(_new);
+    require(isMinter(_new), "address(this) is must be minter");
+  }
+
+  function kill() external onlyOperator() {
+    selfdestruct(owner);
   }
 
   function mint(uint256[] calldata _tokenIds, address[] calldata _owners) external onlyOperator() {
     require(_tokenIds.length == _owners.length);
     for (uint256 i = 0; i < _tokenIds.length; i++) {
-      Alice.mint(_owners[i], _tokenIds[i]);
+      erc721.mint(_owners[i], _tokenIds[i]);
     }
   }
 
