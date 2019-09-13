2f573a25c982996ad2c78bc6e8e313c18e15c221
Ryo Manzoku <manzoku.ryo@gmail.com>
2019-09-13 17:10:43 +0900

    MyNFT

---
+++ contracts/solc0.4/MyNFT.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.20;
+
+import "./ERC721Metadata.sol";
+
+contract MyNFT is ERC721Metadata {
+
+  constructor (string name, string symbol) public ERC721Metadata(name, symbol) {
+  }
+
+  function mint(address _to, uint256 _tokenId) public {
+    _mint(_to, _tokenId);
+  }
+
+  function exist(uint256 _tokenId) public view returns (bool){
+    return _exist(_tokenId);
+  }
+  
+  function updateTokenURIPrefix(string _new) public {
+    _updateTokenURIPrefix(_new);
+  }
+
+}
